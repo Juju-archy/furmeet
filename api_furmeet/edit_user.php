aa1ce8f94247e613387df870e53810416cf3f642
Juju-archy <pouny.julie@gmail.com>
2024-03-14 21:57:21 +0100

    modif

--- api_furmeet/edit_user.php
+++ api_furmeet/edit_user.php
@@ -10,8 +10,6 @@ require_once("hash_crypto.php");
 // Vérifie que les champs sont présents dans la requête GET
 if (isset($_GET['uemail'])) {
     $email = $_GET['uemail'];
-    $response['message'] = "l'email est ".$email;
-    echo json_encode($response);
 
     if (empty($email)) {
         // Si 'uemail' est vide, renvoyer une réponse d'erreur
